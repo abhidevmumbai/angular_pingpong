2df9352b64b1b4adaf1aaeaadf320a0730f4db2d
abhishek <abhidevmumbai@gmail.com>
2018-07-05 20:11:13 -0400

    Added game CRUD service methods

--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -9,7 +9,22 @@ export class GameService {
 	constructor(private http: Http) { }
 
 	getGames() {
-		return this.http.get(environment.apiURL + "games", ).map((res:Response) => res.json());	
+		return this.http.get(`${environment.apiURL}/games`, ).map((res:Response) => res.json());	
 	}
 
+	getGame(id) {
+		return this.http.get(`${environment.apiURL}/games/${id}`, ).map((res:Response) => res.json());	
+	}
+
+	createGame(params) {
+		return this.http.post(`${environment.apiURL}/games/create`, params).map((res:Response) => res.json());	
+	}
+
+	editGame(id, params) {
+		return this.http.put(`${environment.apiURL}/games/${id}`, params).map((res:Response) => res.json());	
+	}
+
+	deleteGame(id) {
+		return this.http.delete(`${environment.apiURL}/games/${id}`).map((res:Response) => res.json());	
+	}	
 }
