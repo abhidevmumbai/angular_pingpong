ac7c572f0d4de7b09b54abbebafe5c90df1b45fa
abhishek <abhidevmumbai@gmail.com>
2018-07-06 21:18:54 -0400

    Updated angular to 6

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 
 import { AppComponent } from './app.component';
 import { GamesComponent } from './games/games.component';
 import { GameDetailsComponent } from './game-details/game-details.component';
-
-
 
 let routes = [{
     path: 'games',
@@ -32,7 +29,7 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot(routes, { useHash:
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     rootRouting
   ],
   providers: [],
--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../environments/environment';
 
 @Injectable()
 export class GameService {
-	constructor(private http: Http) { }
+	constructor(private http: HttpClient) { }
 
 	getGames() {
-		return this.http.get(`${environment.apiURL}/games`, ).map((res:Response) => res.json());	
+		return this.http.get(`${environment.apiURL}/games`);	
 	}
 
-	getGame(id) {
-		return this.http.get(`${environment.apiURL}/games/${id}`, ).map((res:Response) => res.json());	
-	}
+	// getGame(id) {
+	// 	return this.http.get(`${environment.apiURL}/games/${id}`, ).map((res:Response) => res.json());	
+	// }
 
-	createGame(params) {
-		return this.http.post(`${environment.apiURL}/games/create`, params).map((res:Response) => res.json());	
-	}
+	// createGame(params) {
+	// 	return this.http.post(`${environment.apiURL}/games/create`, params).map((res:Response) => res.json());	
+	// }
 
-	editGame(id, params) {
-		return this.http.put(`${environment.apiURL}/games/${id}`, params).map((res:Response) => res.json());	
-	}
+	// editGame(id, params) {
+	// 	return this.http.put(`${environment.apiURL}/games/${id}`, params).map((res:Response) => res.json());	
+	// }
 
-	deleteGame(id) {
-		return this.http.delete(`${environment.apiURL}/games/${id}`).map((res:Response) => res.json());	
-	}	
+	// deleteGame(id) {
+	// 	return this.http.delete(`${environment.apiURL}/games/${id}`).map((res:Response) => res.json());	
+	// }	
 }
